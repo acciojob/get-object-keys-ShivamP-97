6c2d318e2ef4e47ec1d66f2874a37d78e70f0666
ShivamP-97 <shivampathak19972@gmail.com>
2026-01-07 19:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+  age:30,
+  city:"New York",
+};
+
+Object.prototype.getKeys=function(){
+	return Object.keys(this)
+}
+console.log(student.getKeys());
